e9e0a5af12df8a3fae47f026b596bb02d93952fc
young <poh1207@gmail.com>
2023-08-16 14:12:15 +0900

    A

--- function.py
+++ function.py
@@ -2,24 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
+#### hyper parameter
+epoch=70
+trainData=100
+inputLength=7
+numOfPrediction=50
+
 #### what we want to predict
 def function(x):
     # return np.power(1.15,x)
-    # return x*np.sin(x)
+    return x*np.sin(x)
     # return x**3
-    return np.log(x)
+    # return np.log(x)
 
 #### data preprocessing
-trainData=50
-inputLength=5
-predictionSize=30
 a=[]
 x=[]
 x_train=[]
 x_test=[]
 y_real=[]
 y_train=[]
-for i in range(1,trainData + predictionSize):
+for i in range(1,trainData + numOfPrediction):
     for j in range(i,i+inputLength):
         a.append( function(j) )
     y_real.append( function(i+inputLength) )
@@ -37,11 +40,10 @@ y_train = np.array(y_train, dtype='f')
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
 
-#2. model build-up
+#### model build-up
 from keras.models import Sequential
-from keras.layers import Dense, SimpleRNN, LSTM
+from keras.layers import Dense, LSTM
 model = Sequential()
-# model.add(SimpleRNN(7, input_shape = (inputLength, 1), activation ='relu'))
 model.add(LSTM(7, input_shape = (inputLength, 1), activation ='relu'))
 model.add(Dense(4))
 model.add(Dense(4))
@@ -50,12 +52,12 @@ model.add(Dense(4))
 model.add(Dense(1))
 model.summary()
 
-#3. training
+#### training
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-history = model.fit(x_train, y_train, epochs=70, batch_size=1)
+history = model.fit(x_train, y_train, epochs=epoch, batch_size=1)
 
-#4. prediction
-for i in range(predictionSize):
+#### prediction
+for i in range(numOfPrediction):
     prediction = model.predict(x_test)[0][0] 
     y_predict.append(prediction)
     x_test = x_test.flatten()
@@ -63,7 +65,6 @@ for i in range(predictionSize):
         x_test[j]= x_test[j+1]
     x_test[ len(x_test)-1 ] = prediction
     x_test = x_test.reshape(1, x_test.shape[0], 1)
-
     
 plt.plot(history.history["loss"])
 plt.show()
